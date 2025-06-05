719a014f3963f19548c12765c9c86c9ac333e7b0
deepak.b <deepak.b@smartxtech.co>
2025-06-05 19:28:04 +0530

    superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# ✅ Create superuser if not already created
+echo "from django.contrib.auth import get_user_model; \
+      User = get_user_model(); \
+      User.objects.filter(username='admin').exists() or \
+      User.objects.create_superuser('admin', 'admin@example.com', 'Test@12345')" \
+| python manage.py shell
